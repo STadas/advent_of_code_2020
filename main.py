b5cf02dca7d844222c20abc34f560e613064b35b
Tadas <saltenis.tadas@gmail.com>
2020-12-04 15:42:05 +0200

    fixed main crash, the pic was just too funny i guess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+#!/bin/python3
 import os, importlib
-dirs = [f.name for f in os.scandir(os.path.dirname(os.path.abspath(__file__))) if f.is_dir() and f.name[0] != '.']
+dirs = [f.name for f in os.scandir(os.path.dirname(os.path.abspath(__file__))) if f.is_dir() and f.name[0] != "." and f.name != "funnies"]
 dirs.sort()
 for d in dirs:
 	print("------", d, "------")
