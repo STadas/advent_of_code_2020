4efcd298ff220000950a419d03687b4efffb932d
TSalten <saltenis.tadas@gmail.com>
2021-12-01 02:07:24 +0200

    day16 part 2 for an extremely rusty warmup

--- day16/day16.py
+++ day16/day16.py
@@ -2,35 +2,66 @@ import os, re
 
 
 def readfile(input):
-	with open(os.path.dirname(os.path.abspath(__file__)) + '/' + input, 'r') as f:
-		data = f.read()
-		f.close()
-	return data.splitlines()
+    with open(os.path.dirname(os.path.abspath(__file__)) + '/' + input, 'r') as f:
+        data = f.read()
+        f.close()
+    return data.splitlines()
 
 
 def anyways(data):
-	ranges = []
-	valid = []
-	attrs = 0
-	p1 = 0
-	for i, line in enumerate(data):
-		if "-" in line:
-			attrs += 1
-			pattern = re.compile("([0-9]+)-([0-9]+) or ([0-9]+)-([0-9]+)")
-			res = pattern.search(line)
-			min1, max1, min2, max2 = [int(num) for num in res.groups()]
-			ranges.extend([[min1, max1], [min2, max2]])
-		elif "," in line and data[i-1][:4] != "your":
-			vals = [int(n) for n in line.split(",")]
-			bad = False
-			for num in vals:
-				if not any(num in range(x, y+1) for x, y in ranges):
-					bad = True
-					p1 += num
-			if not bad and vals not in valid:
-				valid.append(vals)
-	print("part1", p1)
-	print("part2", "looks tedious as hell cba")
+    ranges = []
+    valid = set()
+    attrs = []
+    p1 = 0
 
+    for i, line in enumerate(data):
+        if "-" in line:
+            attrs.append(line[:line.find(":")])
+
+            pattern = re.compile("(\d+)-(\d+) or (\d+)-(\d+)")
+            p_res = list(map(int, pattern.search(line).groups()))
+            ranges.extend(((p_res[0], p_res[1]), (p_res[2], p_res[3])))
+
+        elif "," in line and not "your" in data[i-1]:
+            vals = tuple(map(int, line.split(",")))
+            bad = False
+
+            for v in vals:
+                if not any(v in range(x, y+1) for x, y in ranges):
+                    p1 += v
+                    bad = True
+
+            if not bad:
+                valid.add(vals)
+        elif "," in line and "your" in data[i-1]:
+            your_ticket = tuple(map(int, line.split(",")))
+    
+    tickets_res = []
+    for vals in valid:
+        ticket_fits = []
+        for v in vals:
+            value_fits = set(int(i // 2) for i, r in enumerate(ranges) if v in range(r[0], r[1]+1))
+            ticket_fits.append(value_fits)
+        tickets_res.append(ticket_fits)
+
+    p2_order = tickets_res[0]
+    for ticket in tickets_res[1:]:
+        for i in range(len(p2_order)):
+            p2_order[i] = p2_order[i] & ticket[i]
+
+    used = set()
+    for i, v in sorted(zip(range(len(p2_order)), p2_order), key=lambda x: len(x[1])):
+        p2_order[i] = list(v - used)[0]
+        used.update(v)
+
+    p2_order = [attrs[attr_i] for attr_i in p2_order]
+    p2_departure_indexes = [i for i, a in enumerate(p2_order) if "departure" in a]
+    
+    p2 = 1
+    for i in p2_departure_indexes:
+        p2 *= your_ticket[i]
+
+    print(f"{p1=}")
+    print(f"{p2=}")
 
 anyways(readfile("input"))
